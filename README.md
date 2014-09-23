3e04a0925bc6aa3768b96b6b03fe2ea00bbe38ea
binx <rachelbinx@gmail.com>
2014-09-23 16:53:09 -0400

    cleaning up the syntax, adding readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 WiFiDiary
 =========
+# Setup
+1. Install imagesnap
+	
+		brew install imagesnap
+	
+2. Set up your chron job
+
+		$ crontab -e
+
+	I set mine up to run every 20 minutes:
+	
+		0,20,40 * * * * /path/to/repo/check_wifi.sh
+
+# Optional Tumblr Upload
+1. Uncomment line 19 in chech_wifi.sh
+2. Register an app at the [Tumblr API](https://www.tumblr.com/docs/en/api/v2) for the Consumer Key and Consumer Secret
+3. Use the [Tumblr Console](https://api.tumblr.com/console) to generate your OAuth Key and OAuth Secret
+
+# Voila!
+Now you too can have a record of your computer-face.
+![](http://33.media.tumblr.com/91ab16582ec3e9103aa77d8f1c4aabb6/tumblr_nc82suIKUT1tmz7aco1_1280.jpg)
--- check_wifi.sh
+++ check_wifi.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-AIR=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep ' SSID' | cut -d ':' -f 2)
-AIR=${AIR//[[:blank:]]/}
+SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep ' SSID' | cut -d ':' -f 2)
+SSID=${SSID//[[:blank:]]/}
 
 FILEPATH="/path/to/repo/"
-FILENAME='.jpg'
 
 FILE="${FILEPATH}SSID.txt"
 SAVED=`cat ${FILEPATH}SSID.txt`
@@ -12,11 +11,12 @@ SAVED=${SAVED//[[:blank:]]/}
 
 echo $SAVED
 
-if [ "$AIR" != "$SAVED" ]; then
-	/usr/local/bin/imagesnap "${FILEPATH}photos/${AIR}${FILENAME}"
-	echo "$AIR" > "$FILE"
+if [ "$SSID" != "$SAVED" ]; then
+	DATE=$(date +"%Y%m%d%H%M")
+	/usr/local/bin/imagesnap "${FILEPATH}photos/${DATE}${SSID}.jpg"
+	echo "$SSID" > "$FILE"
 	sleep 5
-	python "${FILEPATH}tumblr.py" $AIR
+#	python "${FILEPATH}tumblr.py" $SSID
 fi
 
 exit 0
