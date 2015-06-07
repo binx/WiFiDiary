e68a54c23226b7f2532f9af423f00a2a2727429b
Alan Palazzolo <apalazzolo@minnpost.com>
2015-06-07 16:27:20 -0400

    Adding path to be configurable.

--- check_wifi.sh
+++ check_wifi.sh
@@ -3,9 +3,15 @@
 SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep ' SSID' | cut -d ':' -f 2)
 SSID=${SSID//[[:blank:]]/}
 
-FILEPATH="/path/to/repo/"
+# Get path from argument (or use current path)
+FILEPATH=$1
+if [ -z "$FILEPATH" ]
+then
+	FILEPATH="./"
+fi
 
 FILE="${FILEPATH}SSID.txt"
+touch $FILE
 SAVED=`cat ${FILEPATH}SSID.txt`
 SAVED=${SAVED//[[:blank:]]/}
 
@@ -13,7 +19,7 @@ echo $SAVED
 
 if [ "$SSID" != "$SAVED" ]; then
 	DATE=$(date +"%Y%m%d%H%M")
-	/usr/local/bin/imagesnap "${FILEPATH}photos/${DATE}${SSID}.jpg"
+	/usr/local/bin/imagesnap "${FILEPATH}photos/${DATE}-${SSID}.jpg"
 	echo "$SSID" > "$FILE"
 	sleep 5
 #	python "${FILEPATH}tumblr.py" $DATE $SSID
